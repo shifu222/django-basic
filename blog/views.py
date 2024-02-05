77192ee9f541a95a3cf00f41fd41439d183a1606
Shifu <gabrielcastillejo25@gmail.com>
2024-02-04 21:43:58 -0500

    create form

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.views.generic import View
+from .models import Post
+from .forms import PostCreateForm
+class BlogListView(View):
+    def get(self,request,*args,**kwargs):
+        context={
+            
+        }
+        return render(request,'blog_list.html',context)
+ 
+class BlogCreateView(View):
+     def get(self,request,*args,**kwargs):
+         return render(request,"create_post.html",{'form':PostCreateForm()})
+        
+     def post(self,request,*args,**kwargs):
+         if request.method == "POST":
+             form = PostCreateForm(request.POST)
+             if form.is_valid():
+                 title = form.cleaned_data.get('title')
+                 content = form.cleaned_data.get('content')
+                 p,created = Post.objects.get_or_create(title=title,content=content)
+                 p.save()
+                 return redirect('blog:create')         
